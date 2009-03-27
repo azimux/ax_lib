41506fe875f150a1308ac0ef5887423b2228bca7
Miles Georgi <azimux@gmail.com>
2009-03-27 04:39:47 -0700

    added assert_no_difference assertion

--- lib/azimux/ext/test/unit/assertions.rb
+++ lib/azimux/ext/test/unit/assertions.rb
@@ -1,10 +1,8 @@
 require 'test/unit/assertions'
 module Test
   module Unit
-    #--
-    # FIXME: no Proc#binding in Ruby 2, must change this API
-    #++
     module Assertions
+      #based on assert_difference from rails
       def assert_changed(expressions, message = nil, &block)
         expression_evaluations = Array(expressions).map do |expression|
           lambda do
@@ -18,6 +16,21 @@ module Test
           assert_not_equal original_values[i], expression.call, message
         end
       end
+
+      def assert_not_changed(expressions, message = nil, &block)
+        expression_evaluations = Array(expressions).map do |expression|
+          lambda do
+            eval(expression, block.__send__(:binding))
+          end
+        end
+
+        original_values = expression_evaluations.inject([]) { |memo, expression| memo << expression.call }
+        yield
+        expression_evaluations.each_with_index do |expression, i|
+          assert_equal original_values[i], expression.call, message
+        end
+      end
+
     end
   end
 end
